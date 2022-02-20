18f53b8794e3f0f163dae9bf4fb94bb641803baf
christiangsn <christianguimaraes1996@gmail.com>
2022-02-20 17:29:51 -0300

    feat: adicionado menu e sidebar no dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,30 @@
+import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react";
+
 import { Header } from "../components/Header";
+import { SideBar } from "../components/Sidebar";
 
 export default function Dashboard () {
     return (
-        <Header />
+        <Flex direction="column" h="100vh"> 
+            <Header />
+
+            <Flex w="100%" maxWidth={1480} mx="auto" px="6">
+                <SideBar />
+
+                <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start"> 
+
+                    <Box p="8" bg="gray.800" borderRadius={8} minHeight="340px">
+                        <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+                    </Box>
+
+                    <Box p="8" bg="gray.800" borderRadius={8} minHeight="340px">
+                        <Text fontSize="lg" mb="4">Taxa de Abertura</Text>
+                    </Box>
+
+            
+                </SimpleGrid>
+            </Flex>
+        </Flex>
+        
     )
 }
